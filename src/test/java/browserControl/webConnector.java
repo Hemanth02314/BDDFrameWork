3f8eb54823e134198c7c28b9f332e021a5c76a42
hemanth <hemanth2314@gmail.com>
2024-06-24 17:32:39 +0100

    Merge remote-tracking branch 'origin/master'

--- src/test/java/browserControl/webConnector.java
+++ src/test/java/browserControl/webConnector.java
@@ -18,6 +18,9 @@ public class webConnector {
         } else {
             ChromeOptions co = new ChromeOptions();
             co.addArguments("start-maximized");
+            if (ConstantUtils.BROWSER_TYPE.equalsIgnoreCase("headless")){
+                co.addArguments("headless");
+            }
             driver = new ChromeDriver(co);
         }
 
